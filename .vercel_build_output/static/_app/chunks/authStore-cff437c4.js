4dd5dc4a6c7766d5267c4bc95378e85d4cd20ceb
Mohit RAJ PUROHIT <mpurohit2@sapient.com>
2022-04-13 23:31:28 +0530

    Deleted vercel files

--- .vercel_build_output/static/_app/chunks/authStore-cff437c4.js
+++
@@ -1,1 +0,0 @@
-import{w as s}from"./index-6ffe4708.js";const e=s({isLoggedIn:!1,firebaseControlled:!1});var t={subscribe:e.subscribe,set:e.set};export{t as a};
--- .vercel_build_output/static/_app/chunks/teamsStore-8b76f3b1.js
+++
@@ -1,1 +0,0 @@
-import{w as t}from"./index-6ffe4708.js";const s=t({teams:[]});var r={subscribe:s.subscribe,set:s.set};export{r as t};
